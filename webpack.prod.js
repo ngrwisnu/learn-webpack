0105c6f808e3c527130441ec557e93de1e60ad45
ngrwisnu <adiprayogongrwisnu@gmail.com>
2023-04-19 11:02:46 +0700

    assets management

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,12 +1,19 @@
 const path = require("path");
 const config = require("./webpack.config");
 const { merge } = require("webpack-merge");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = merge(config, {
   mode: "production", // value => 'production' | 'development'
   output: {
     path: path.resolve(__dirname, "output"),
     filename: "bundle.[contenthash].js",
+    assetModuleFilename: "img/[hash][ext]",
     clean: true,
   },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "bundle.[contenthash].css",
+    }),
+  ],
 });
